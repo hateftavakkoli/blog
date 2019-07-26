6910ac1233a4d169eb42d6a5e3f87918a314792d
Hatef tavakkoli <tavakkoli.ht@gmail.com>
2019-07-26 19:14:34 +0430

    hatef

--- src/main/java/ir/arcademy/blog/module/posts/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package ir.arcademy.blog.module.posts.repository;
-
-import ir.arcademy.blog.module.posts.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,Long> {
-}
--- src/main/java/ir/arcademy/blog/module/posts/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package ir.arcademy.blog.module.posts.repository;
-
-import ir.arcademy.blog.module.posts.model.Posts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Posts,Long> {
-
-}
