2167b7154a4ea71088f570a48b29fd93a7b0ef76
Hatef tavakkoli <tavakkoli.ht@gmail.com>
2019-08-25 15:22:00 +0430

    hatef

--- src/main/java/ir/arcademy/blog/modules/MainController.java
+++ src/main/java/ir/arcademy/blog/modules/MainController.java
@@ -1,9 +1,6 @@
 package ir.arcademy.blog.modules;
 
-import ir.arcademy.blog.modules.posts.service.PostsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
