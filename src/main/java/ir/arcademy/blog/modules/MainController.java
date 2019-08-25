6b051790feed286e836c14d1f716d6a75475533e
Hatef tavakkoli <tavakkoli.ht@gmail.com>
2019-08-25 15:17:56 +0430

    hatef

--- src/main/java/ir/arcademy/blog/modules/MainController.java
+++ src/main/java/ir/arcademy/blog/modules/MainController.java
@@ -1,13 +1,24 @@
 package ir.arcademy.blog.modules;
 
+import ir.arcademy.blog.modules.posts.service.PostsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
+    private PostsService postsService;
+
+    @Autowired
+    public MainController(PostsService postsService) {
+        this.postsService = postsService;
+    }
+
     @RequestMapping("")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllPosts());
         return "index";
     }
 
