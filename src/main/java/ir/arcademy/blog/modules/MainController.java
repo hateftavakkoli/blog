9a69b42adf1b2fedd281de2bbb4a7da6fb4de313
Hatef tavakkoli <tavakkoli.ht@gmail.com>
2019-08-29 15:49:36 +0430

    hatef

--- src/main/java/ir/arcademy/blog/modules/MainController.java
+++ src/main/java/ir/arcademy/blog/modules/MainController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
