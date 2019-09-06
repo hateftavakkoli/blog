bd8c1d2e6f0fb36de6e29683d39f0ffb10a4d92f
Hatef tavakkoli <tavakkoli.ht@gmail.com>
2019-09-06 19:17:11 +0430

    hatef

--- src/main/java/ir/arcademy/blog/config/SpringSecurityConfig.java
+++ src/main/java/ir/arcademy/blog/config/SpringSecurityConfig.java
@@ -1,20 +1,12 @@
 package ir.arcademy.blog.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private DataSource dataSource;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
